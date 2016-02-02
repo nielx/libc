63325b738e4eb3e3a87369db08e321cfe9efd5ed
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2016-02-02 21:21:36 +0100

    Add support for Haiku x86 and x86_64

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -148,6 +148,10 @@ cfg_if! {
         #[link(name = "c")]
         #[link(name = "m")]
         extern {}
+    } else if #[cfg(target_os = "haiku")] {
+        #[link(name = "root")]
+        #[link(name = "network")]
+        extern {}
     } else {
         #[link(name = "c")]
         #[link(name = "m")]
@@ -572,7 +576,9 @@ extern {
 // TODO: get rid of this #[cfg(not(...))]
 #[cfg(not(target_os = "android"))]
 extern {
+    #[cfg(not(target_os = "haiku"))]
     pub fn getifaddrs(ifap: *mut *mut ifaddrs) -> ::c_int;
+    #[cfg(not(target_os = "haiku"))]
     pub fn freeifaddrs(ifa: *mut ifaddrs);
     #[cfg_attr(target_os = "macos", link_name = "glob$INODE64")]
     #[cfg_attr(target_os = "netbsd", link_name = "__glob30")]
@@ -697,6 +703,9 @@ cfg_if! {
                         target_os = "bitrig"))] {
         mod bsd;
         pub use self::bsd::*;
+    } else if #[cfg(target_os = "haiku")] {
+        mod haiku;
+        pub use self::haiku::*;
     } else {
         // ...
     }
